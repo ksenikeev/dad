b3d2de1385414d4b5222aafaa36828c048ac18f2
ksenikeev <ksenikeev@mail.ru>
2018-11-05 08:52:38 +0300

    Insert, delete, update, select  examples

--- src/icmit/dad/laborators/lab1/Insert2Example.java
+++ src/icmit/dad/laborators/lab1/Insert2Example.java
@@ -2,23 +2,43 @@ package icmit.dad.laborators.lab1;
 
 import java.sql.*;
 
-public class InsertExample {
+/**
+ * Пример добавления данных в 2 связанные таблицы: contact и phone.
+ * Связь между таблицами осуществляется посредством внешнего ключа
+ * contact_id таблицы phone.
+ *
+ *
+ */
+public class Insert2Example {
     public static void main(String[] args) {
         String url = "jdbc:postgresql://localhost:5432/dad";
         try (Connection conn = DriverManager.getConnection(url, "postgres", "post")) {
-            Statement statement = conn.createStatement();
+            Statement s = conn.createStatement();
 
-            ResultSet rs = statement.executeQuery("select nextval('contact_seq') as id");
+            // Генерируем новое значение первичного ключа для таблицы contact,
+            // используя последовательность contact_seq
+            ResultSet rs = s.executeQuery("select nextval('contact_seq') as id");
             long id=0;
             if (rs.next()){ id = rs.getLong("id");}
             rs.close();
 
-            statement.executeUpdate("insert into contact (id, fullname) values ("+id+",'Мой хороший друг 2')");
+            PreparedStatement ps1 =
+                    conn.prepareStatement("insert into contact (id, fullname) values ( ? , ? ) ");
+            ps1.setLong(1, id);
+            ps1.setString(2, "Контакт "+id);
 
-            statement.executeUpdate("insert into phone (contact_id, phonetype_id, phonenumber) values " +
-                    "("+id+", 1, '+7902340459')");
-            statement.close();
-            System.out.println(id);
+            ps1.executeUpdate();
+            ps1.close();
+
+            PreparedStatement ps2 =
+                conn.prepareStatement(
+                    "insert into phone (contact_id, phonetype_id, phonenumber) values ( ? , ? , ? )");
+            ps2.setLong(1, id);
+            ps2.setLong(2, 1);
+            ps2.setString(3, "+7902340459");
+
+            ps2.executeUpdate();
+            ps2.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
