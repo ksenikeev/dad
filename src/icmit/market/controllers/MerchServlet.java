8ea339a2f03c632025f9fcc5cc7a571a4593c8a5
ksenikeev <ksenikeev@mail.ru>
2019-02-07 09:31:06 +0300

    web.xml

--- src/icmit/market/controllers/MerchServlet.java
+++ src/icmit/market/controllers/MerchServlet.java
@@ -2,6 +2,7 @@ package icmit.market.controllers;
 
 import icmit.market.DbWork;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/merch")
 public class MerchServlet extends HttpServlet {
@@ -20,15 +23,10 @@ public class MerchServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response){
 
         Connection con = DbWork.getInstance();
+        List<String> merchList = new ArrayList<String>();
 
-        response.setHeader("Content-Type", "text/html; charset=utf-8");
-        try {
-            PrintWriter pw = response.getWriter();
-            pw.println("<html><head><meta charset=\"utf-8\"/></head><body>");
-            pw.println("<h1>Добро пожаловать</h1>");
-            pw.println("<h2>Наши товары</h2>");
 
-            try{
+           try{
                 String sql = "select * from price";
                 Statement st = con.createStatement();
                 ResultSet rs = st.executeQuery(sql);
@@ -37,20 +35,19 @@ public class MerchServlet extends HttpServlet {
                     String name = rs.getString("name");
                     String price = rs.getString("pricevalue");
                     String available = rs.getBoolean("available")?"Есть в наличии":"Нет в наличии";
-                    pw.println(id +", "+name+", "+price+", "+available+"<br/>");
+
+                    merchList.add("id: "+id+", name: "+name + ", price: "+price);
                 }
 
+               request.setAttribute("merchList", merchList);
+                request.getRequestDispatcher("merch.ftlh").forward(request,response);
             } catch (SQLException e) {
                 e.printStackTrace();
-            }
-
-            pw.println("<a href='index.html'>Главная</a>");
-
-            pw.println("</body></html>");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            } catch (ServletException e) {
+               e.printStackTrace();
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
 
     }
 }
